8f28c0004249247b557a8d7f08780885c497e728
61778 <helloworldwh@163.com>
2026-07-15 17:15:14 +0800

    chore(build): 更新程序集信息配置
    - 修改程序集标题为 opc_ae_relay
    - 更新程序集描述为通用 OPC AE 告警采集与转发工具
    - 设置程序集配置为 Release
    - 更新产品名称为 opc_ae_relay
    - 更改版权信息为开发者 wanghao
    - 添加 ComVisible 注释说明，明确设置为 false 的原因

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,18 +4,20 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("opcLearn")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+// 程序集常规信息
+[assembly: AssemblyTitle("opc_ae_relay")]
+[assembly: AssemblyDescription("通用 OPC AE 告警采集与转发工具")]
+[assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("opcLearn")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyProduct("opc_ae_relay")]
+[assembly: AssemblyCopyright("开发者 wanghao · Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
+// false 将对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+// 请将此类型的 ComVisible 特性设置为 true。
+// 我是opc客户端不对外提供com 所以是false
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
